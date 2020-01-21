5029ca5b2c83701ab8bc1929211d0b50815091ed
bajud124 <59533331+bajud124@users.noreply.github.com>
2020-01-21 17:32:03 +0530

    Add files via upload

--- index1.js
+++ index1.js
@@ -1,64 +1,57 @@
 ﻿$(document).ready(function () {
     $('.trending-project').owlCarousel({
-        loop: true,
+        nav: true,
+        dots: false,
         margin: 10,
         navText: ['<img class="arrow-left" src="images/arrow-toggle.png" >', '<img class="arrow-right" src="images/arrow-toggle.png" />'],
         responsiveClass: false,
         responsive: {
             0: {
                 items: 1,
-                nav: true
             },
             600: {
                 items: 2,
-                nav: true
             },
             1000: {
                 items: 4,
-                nav: true,
-                loop: true
             }
         }
     });
+
+
     $('.feature-destination').owlCarousel({
-        loop: true,
+        nav: true,
+        dots: false,
         margin: 10,
         navText: ['<img class="arrow-left" src="images/arrow-toggle.png" >', '<img class="arrow-right" src="images/arrow-toggle.png" />'],
         responsiveClass: false,
         responsive: {
             0: {
                 items: 2,
-                nav: true
             },
             600: {
                 items: 3,
-                nav: true
             },
             1000: {
                 items: 6,
-                nav: true,
-                loop: true
             }
         }
     });
     $('.affordable-project').owlCarousel({
-        loop: true,
+        nav: true,
+        dots: false,
         margin: 10,
         navText: ['<img class="arrow-left" src="images/arrow-toggle.png" >', '<img class="arrow-right" src="images/arrow-toggle.png" />'],
         responsiveClass: false,
         responsive: {
             0: {
                 items: 1,
-                nav: true
             },
             600: {
                 items: 3,
-                nav: true
             },
             1000: {
                 items: 5,
-                nav: true,
-                loop: true
             }
         }
     });
